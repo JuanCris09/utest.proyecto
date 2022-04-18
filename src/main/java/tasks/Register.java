3bbf587f91bc2e0b3f4869b0f1425ecb5f35d50a
JuanCris09 <juampiscris98@gmail.com>
2022-04-18 08:36:39 -0500

    first commit

--- src/main/java/tasks/Register.java
+++ src/main/java/tasks/Register.java
@@ -1,24 +1,33 @@
 package tasks;
 
 import net.serenitybdd.screenplay.Actor;
-import net.serenitybdd.screenplay.Performable;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.Tasks;
 import net.serenitybdd.screenplay.actions.Click;
 import net.serenitybdd.screenplay.actions.Enter;
-import userinterface.UtestLoginPage;
+import userinterface.UtestRegisterPage;
 
-public class Login implements Task {
-    public static Login onThePage() {
+public class Register implements Task {
+    private String strEmail;
 
-        return Tasks.instrumented(Login.class);
+    public Register(String strEmail) {
+        this.strEmail = strEmail;
+    }
+
+    public static Register onThePage(String strEmail) {
+
+        return Tasks.instrumented(Register.class, strEmail);
     }
 
     @Override
     public <T extends Actor> void performAs(T actor) {
-        actor.attemptsTo(Click.on(UtestLoginPage.LOGIN_BUTTON),
-                Enter.theValue("IngresarTuUsuario").into(UtestLoginPage.INPUT_USER),
-                Enter.theValue("IngresarTuContraseña").into(UtestLoginPage.INPUT_PASSWORD),
-                Click.on(UtestLoginPage.ENTER_BUTTON));
+        actor.attemptsTo(Click.on(UtestRegisterPage.LOGIN_BUTTON),
+                Enter.theValue("IngresarTuNombre").into(UtestRegisterPage.INPUT_NAME),
+                Enter.theValue("IngresarTuApellido").into(UtestRegisterPage.INPUT_LASTNAME),
+                Enter.theValue(strEmail).into(UtestRegisterPage.INPUT_EMAIL),
+                Enter.theValue("IngresarTuMes").into(UtestRegisterPage.INPUT_MES),
+                Enter.theValue("IngresarTuDia").into(UtestRegisterPage.INPUT_DAY),
+                Enter.theValue("IngresarTuAnio").into(UtestRegisterPage.INPUT_YEARS)
+        );
     }
 }
